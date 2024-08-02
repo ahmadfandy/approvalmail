ba00be085d653a37a60a839ae91e4c8e12fff454
andri prasetyo <andri.prasetyo@ifca.co.id>
2024-08-02 14:31:58 +0700

    penambahan log 01/08/2024

--- app/Http/Controllers/EndingController.php
+++ app/Http/Controllers/EndingController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\EndingMail;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Log;
 
 class EndingController extends Controller
 {
